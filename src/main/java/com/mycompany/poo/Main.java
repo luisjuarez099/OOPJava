8d677c12b654917dffea37577fc3fbd16caa5572
luis juarez <luisjuarezcc9@gmail.com>
2023-04-03 11:47:09 -0600

    Nueva class

--- src/main/java/com/mycompany/poo/Main.java
+++ src/main/java/com/mycompany/poo/Main.java
@@ -1,8 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.poo;
+
 /**
  *
  * @author luisjc
@@ -11,8 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
        
-        
-        
         
     }
 }
